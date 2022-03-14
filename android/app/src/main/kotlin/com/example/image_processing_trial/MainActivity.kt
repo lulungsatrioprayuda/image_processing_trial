040b1a764dd3a49d42d44e901fe42a4beb6bc303
lulungsatrioprayuda <alungsatrio12@gmail.com>
2022-03-14 19:14:25 +0800

    first upload

---
+++ android/app/src/main/kotlin/com/example/image_processing_trial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_processing_trial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.lime),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
